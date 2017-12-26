3756c4f8f1376bb1b9ebcd52e266dc7a9711ae07
Nicolas RAYNAUD <x9nico2@outlook.fr>
2017-12-26 10:59:28 +0100

    Tried to fix GameAliveFactory error 2

---
+++ src/fr/badblock/bukkit/hub/v2/rabbit/GameAliveFactory.java
@@ -0,0 +1,12 @@
+package fr.badblock.bukkit.hub.v2.rabbit;
+
+public class GameAliveFactory {
+
+	String name;
+	
+	public String getName() {
+		// TODO Auto-generated method stub
+		return name;
+	}
+
+}
