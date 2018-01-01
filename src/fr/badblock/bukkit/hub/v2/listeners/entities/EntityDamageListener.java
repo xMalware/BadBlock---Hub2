9eb9851b08162c9e94c07759c7388e34170d8b94
Nicolas RAYNAUD <x9nico2@outlook.fr>
2018-01-01 11:33:16 +0100

    Blocking all entities damages

--- src/fr/badblock/bukkit/hub/v2/listeners/entities/EntityDamageListener.java
+++ src/fr/badblock/bukkit/hub/v2/listeners/entities/EntityDamageListener.java
@@ -1,6 +1,8 @@
 package fr.badblock.bukkit.hub.v2.listeners.entities;
 
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDamageByBlockEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 
 import fr.badblock.gameapi.BadListener;
@@ -9,6 +11,16 @@ public class EntityDamageListener extends BadListener{
 	
 	@EventHandler
 	public void damage(EntityDamageEvent e) {
+		e.setCancelled(true);
+	}
+	
+	@EventHandler
+	public void damage1(EntityDamageByEntityEvent e) {
+		e.setCancelled(true);
+	}
+	
+	@EventHandler
+	public void damage2(EntityDamageByBlockEvent e) {
 		e.setCancelled(true);
 	}
 
