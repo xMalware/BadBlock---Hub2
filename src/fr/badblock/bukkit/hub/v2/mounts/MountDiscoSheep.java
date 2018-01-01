0b6329898ac098e53d963e6cfae1220e80e7e2af
Nicolas RAYNAUD <x9nico2@outlook.fr>
2018-01-01 11:12:32 +0100

    Add Mounts of Sheep is upside down and a DiscoSheep :D

---
+++ src/fr/badblock/bukkit/hub/v2/mounts/MountDiscoSheep.java
@@ -0,0 +1,96 @@
+package fr.badblock.bukkit.hub.v2.mounts;
+
+import java.lang.reflect.Field;
+import org.bukkit.Location;
+import org.bukkit.craftbukkit.v1_8_R3.CraftWorld;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftLivingEntity;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftSheep;
+import org.bukkit.entity.Sheep;
+import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
+import net.minecraft.server.v1_8_R3.EntityHuman;
+import net.minecraft.server.v1_8_R3.EntityInsentient;
+import net.minecraft.server.v1_8_R3.EntityLiving;
+import net.minecraft.server.v1_8_R3.EntitySheep;
+import net.minecraft.server.v1_8_R3.EnumColor;
+import net.minecraft.server.v1_8_R3.GenericAttributes;
+import net.minecraft.server.v1_8_R3.MathHelper;
+import net.minecraft.server.v1_8_R3.World;
+
+public class MountDiscoSheep extends EntitySheep{
+
+	protected Field FIELD_JUMP = null;
+	
+	public MountDiscoSheep(World world) {
+		super(world);
+		if(FIELD_JUMP == null) {
+			try {
+				FIELD_JUMP = EntityLiving.class.getDeclaredField("aY");
+				FIELD_JUMP.setAccessible(true);
+			} catch(NoSuchFieldException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	@Override
+    public void g(float f, float f1) {
+        if(this.passenger != null && this.passenger instanceof EntityHuman)
+        {
+            this.lastYaw = this.yaw = this.passenger.yaw;
+            this.pitch = this.passenger.pitch * 0.5F;
+            this.setYawPitch(this.yaw, this.pitch);
+            this.aK = this.aI = this.yaw;
+            f = ((EntityLiving)this.passenger).aZ * 0.5F;
+            f1 = ((EntityLiving)this.passenger).ba;
+ 
+            if(f1 <= 0.0F)
+            {
+                f1 *= 0.25F;
+            }
+              try {
+                if (FIELD_JUMP.getBoolean(this.passenger) && this.onGround) {
+                    this.motY += 0.5F;
+                    this.ai = true;
+                    if (f1 > 0.0F)
+                    {
+                        float f2 = MathHelper.sin(this.yaw * 3.141593F / 180.0F);
+                        float f3 = MathHelper.cos(this.yaw * 3.141593F / 180.0F);
+ 
+                        this.motX += -0.4F * f2 * ((EntityInsentient) this).br();
+                        this.motZ += 0.4F * f3 * ((EntityInsentient) this).br();
+                    }
+                }
+            } catch (IllegalArgumentException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
+ 
+            this.S = 1.0F; this.aM = this.bI() * 0.1F; if(!this.world.isClientSide)
+            {
+                this.k((float)this.getAttributeInstance(GenericAttributes.MOVEMENT_SPEED).getValue());
+                super.g(f, f1);
+            }
+ 
+            this.aA = this.aB; double d0 = this.locX - this.lastX; double d1 = this.locZ - this.lastZ; float f4 = MathHelper.sqrt(d0 * d0 + d1 * d1) * 4.0F;
+            if(f4 > 1.0F)
+            {
+                f4 = 1.0F;
+            }
+ 
+            this.aB += (f4 - this.aB) * 0.4F; this.aC += this.aB;
+        } else {
+            this.S = 0.5F; this.aM = 0.02F; super.g(f, f1);
+        }
+    }
+	
+	public static Sheep spawnEntity(Location location, EnumColor enumcolor) {
+		World world = (World) ((CraftWorld) location.getWorld()).getHandle();
+		MountDiscoSheep sheep = new MountDiscoSheep(world);
+		sheep.setLocation(location.getX(), location.getY(), location.getZ(), location.getYaw(), location.getPitch());
+		((CraftLivingEntity) sheep.getBukkitEntity()).setRemoveWhenFarAway(false);
+		world.addEntity(sheep, SpawnReason.CUSTOM);
+		sheep.setCustomName("jeb_");
+		sheep.setCustomNameVisible(false);
+		sheep.setColor(enumcolor);
+		return (CraftSheep) sheep.getBukkitEntity();
+	}
+}
--- src/fr/badblock/bukkit/hub/v2/mounts/MountLoader.java
+++ src/fr/badblock/bukkit/hub/v2/mounts/MountLoader.java
@@ -26,6 +26,8 @@ public class MountLoader {
 		NMSUtils.registerEntity("MountVillager", 120, EntityVillager.class, MountVillager.class);
 		NMSUtils.registerEntity("MountEndermite", 67, EntityEndermite.class, MountEndermite.class);
 		NMSUtils.registerEntity("MountSheep", 91, EntitySheep.class, MountSheep.class);
+		NMSUtils.registerEntity("MountSheepSwitched", 91, EntitySheep.class, MountSheepSwitched.class);
+		NMSUtils.registerEntity("MountDiscoSheep", 91, EntitySheep.class, MountDiscoSheep.class);
 		NMSUtils.registerEntity("MountSnowMan", 97, EntitySnowman.class, MountSnowMan.class);
 		NMSUtils.registerEntity("MountWither", 64, EntityWither.class, MountWither.class);
 		NMSUtils.registerEntity("MountRabbit", 101, EntityRabbit.class, MountRabbit.class);
