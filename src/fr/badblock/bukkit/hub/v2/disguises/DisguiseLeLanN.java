478bf5d8f54f2e8cd93e2564c4bd19cdff633259
Nicolas RAYNAUD <x9nico2@outlook.fr>
2018-01-07 19:02:26 +0100

    Added for disguising as BadBlock's admins (for Staff)

---
+++ src/fr/badblock/bukkit/hub/v2/disguises/DisguiseLeLanN.java
@@ -0,0 +1,28 @@
+package fr.badblock.bukkit.hub.v2.disguises;
+
+import org.bukkit.entity.EntityType;
+
+import fr.badblock.gameapi.particles.ParticleEffectType;
+import fr.badblock.gameapi.players.BadblockPlayer;
+
+public class DisguiseLeLanN extends CustomDisguise
+{
+	/**
+	 * For Staff
+	 */
+	
+	BadblockPlayer player;
+	public DisguiseLeLanN(BadblockPlayer player)
+	{
+		super(player, EntityType.PLAYER);
+		player.setCustomName("LeLanN");
+		player.setCustomNameVisible(false);
+	}
+
+	@Override
+	public CustomDisguiseEffect getEffect()
+	{
+		return new CustomDisguiseEffect(ParticleEffectType.CLOUD, 4);
+	}
+
+}
