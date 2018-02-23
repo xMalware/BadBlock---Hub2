2e66384d872a87fc1cb05eac16e9b9e1945b81c9
xMalware <xmalware2@gmail.com>
2018-02-23 15:39:56 +0100

    On fait passer des instructions lors de la connexion pour s'y retrouver +

--- src/fr/badblock/bukkit/hub/v2/listeners/players/PlayerLoadedListener.java
+++ src/fr/badblock/bukkit/hub/v2/listeners/players/PlayerLoadedListener.java
@@ -1,9 +1,7 @@
 package fr.badblock.bukkit.hub.v2.listeners.players;
 
-import java.util.ArrayList;
-
-import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
+
 import fr.badblock.bukkit.hub.v2.players.HubPlayer;
 import fr.badblock.gameapi.BadListener;
 import fr.badblock.gameapi.events.api.PlayerLoadedEvent;
@@ -13,17 +11,26 @@ import fr.badblock.gameapi.utils.i18n.TranslatableString;
 public class PlayerLoadedListener extends BadListener
 {
 	
-	ArrayList<BadblockPlayer> players = new ArrayList<>();
-	
 	@EventHandler
 	public void onPlayerLoaded(PlayerLoadedEvent event)
 	{
 		BadblockPlayer player = event.getPlayer();
+		// Initialize hubPlayer
+		initialize(player);
+		// Broadcast join message
+		broadcastJoinMessage(player);
+	}
+	
+	private void initialize(BadblockPlayer player)
+	{
 		HubPlayer.initialize(player).loadEverything();
-		if(player.hasPermission("hub.broadcastjoin")) {
+	}
+	
+	private void broadcastJoinMessage(BadblockPlayer player)
+	{
+		if (player.hasPermission("hub.broadcastjoin"))
+		{
 			new TranslatableString("hub.broadcast", player.getGroupPrefix(), player.getName()).broadcast();
-		} else {
-			Bukkit.broadcastMessage(null);
 		}
 	}
 	
