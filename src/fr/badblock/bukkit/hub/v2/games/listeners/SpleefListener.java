147244f9523c16e9c0add070f5e57655540f0fcf
Nicolas RAYNAUD <x9nico2@outlook.fr>
2018-03-22 21:06:01 +0100

    Changed Event for get the Defeath of a Player in Spleef

--- src/fr/badblock/bukkit/hub/v2/games/listeners/SpleefListener.java
+++ src/fr/badblock/bukkit/hub/v2/games/listeners/SpleefListener.java
@@ -4,8 +4,9 @@ import fr.badblock.bukkit.hub.v2.games.HubGame;
 import fr.badblock.gameapi.BadListener;
 import fr.badblock.gameapi.players.BadblockPlayer;
 import org.bukkit.GameMode;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 
 import java.util.ArrayList;
 
@@ -14,16 +15,15 @@ public class SpleefListener extends BadListener{
     HubGame game;
     ArrayList<HubGame> gamePlayer = new ArrayList<>();
 
+    //TODO: Get the defeath without moving
     @EventHandler
-    public void falling(EntityDamageEvent event){
-        if(event.getEntity() instanceof BadblockPlayer){
-            BadblockPlayer player = (BadblockPlayer) event.getEntity();
-            if(event.getCause() == EntityDamageEvent.DamageCause.LAVA){
-                if(gamePlayer.contains(player)) {
-                    game.remove(player);
-                    player.sendTranslatedMessage("hub.game.spleef.loose");
-                    player.setGameMode(GameMode.ADVENTURE);
-                }
+    public void move(PlayerMoveEvent event){
+        BadblockPlayer player = (BadblockPlayer) event.getPlayer();
+        if(event.getTo().getBlock().equals(Material.WATER)){
+            if(gamePlayer.contains(player)) {
+                game.remove(player);
+                player.sendTranslatedMessage("hub.game.spleef.loose");
+                player.setGameMode(GameMode.ADVENTURE);
             }
         }
     }
