6131de2412c76c2dade57e7e0ae7401c49eb94bf
Toinetoine1 <toinetoin1@gmail.com>
2019-02-10 19:56:05 +0100

    Refonte doublejump

---
+++ src/fr/badblock/bukkit/hub/v2/listeners/players/DoubleJumpListener.java
@@ -0,0 +1,59 @@
+package fr.badblock.bukkit.hub.v2.listeners.players;
+
+import fr.badblock.bukkit.hub.v2.utils.ParticleEffect;
+import fr.badblock.gameapi.BadListener;
+import fr.badblock.gameapi.players.BadblockPlayer;
+import org.bukkit.GameMode;
+import org.bukkit.Sound;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerToggleFlightEvent;
+
+import java.util.*;
+
+public class DoubleJumpListener extends BadListener {
+
+    private final List<UUID> allowed;
+
+    public DoubleJumpListener() {
+        this.allowed = new ArrayList<>();
+    }
+
+    @EventHandler
+    public void onMove(PlayerMoveEvent event) {
+        BadblockPlayer player = (BadblockPlayer) event.getPlayer();
+
+        if (player.getGameMode() != GameMode.ADVENTURE)
+            return;
+
+        if (player.getAllowFlight())
+            return;
+
+        if (((LivingEntity) player).isOnGround()) {
+            player.setAllowFlight(true);
+            this.allowed.add(player.getUniqueId());
+        }
+    }
+
+    @EventHandler
+    public void onPlayerToggleFlight(PlayerToggleFlightEvent event) {
+        BadblockPlayer player = (BadblockPlayer) event.getPlayer();
+
+        if (!this.allowed.contains(player.getUniqueId()))
+            return;
+
+        this.allowed.remove(player.getUniqueId());
+
+        event.setCancelled(true);
+
+        player.setVelocity(player.getLocation().getDirection().multiply(1.6D).setY(1.0D));
+        player.playSound(player.getLocation(), Sound.ENDERDRAGON_WINGS, 1.0F, 1.0F);
+
+        for (int i = 0; i < 20; i++)
+            ParticleEffect.CLOUD.display(0.5F, 0.15F, 0.5F, 0.25F, 20, player.getLocation().subtract(0.0F, 0.20F, 0.0F), 32);
+
+        player.setAllowFlight(false);
+    }
+
+}
--- src/fr/badblock/bukkit/hub/v2/listeners/players/PlayerMoveListener.java
+++ src/fr/badblock/bukkit/hub/v2/listeners/players/PlayerMoveListener.java
@@ -12,78 +12,56 @@ import fr.badblock.bukkit.hub.v2.players.HubPlayer;
 import fr.badblock.gameapi.BadListener;
 import fr.badblock.gameapi.players.BadblockPlayer;
 
-public class PlayerMoveListener extends BadListener
-{
+public class PlayerMoveListener extends BadListener {
 
 
-	@EventHandler
-	public void whenPlayerMoved(PlayerMoveEvent event)
-	{	
-		
-		BadblockPlayer player = (BadblockPlayer) event.getPlayer();
-		HubPlayer hubPlayer = HubPlayer.get(player);
-		workWithDisguiseEffects(player, hubPlayer);
-		workLaunchpad(player, event.getTo());
-		autoTeleport(player, event);
-		//workWithDoubleJump(player);
-	}
+    @EventHandler
+    public void whenPlayerMoved(PlayerMoveEvent event) {
+        BadblockPlayer player = (BadblockPlayer) event.getPlayer();
+        HubPlayer hubPlayer = HubPlayer.get(player);
 
-	private void autoTeleport(BadblockPlayer player, PlayerMoveEvent event)
-	{
-		Location to = event.getTo();
-		if (to.getY() <= 0)
-		{
-			event.setTo(to.getWorld().getSpawnLocation());
-		}
-	}
-	
-	private void workLaunchpad(BadblockPlayer player, Location to)
-	{
-		Location loc2 = to.clone().add(0, -1, 0);
-		Location loc3 = to.clone().add(0, -2, 0);
-		Block block1 = loc2.getBlock();
-		Block block2 = loc3.getBlock();
-		if (isLaunchpadBlock(block1) || isLaunchpadBlock(block2)) 
-		{
-			player.setVelocity(player.getLocation().getDirection().multiply(20).add(new Vector(0.5, 3, 0.5)));
-		}
-	}
-	
-	@SuppressWarnings("deprecation")
-	private boolean isLaunchpadBlock(Block block)
-	{
-		return block.getType().equals(Material.WOOL) && block.getData() == 10;
-	}
-	
-	private void workWithDisguiseEffects(BadblockPlayer player, HubPlayer hubPlayer)
-	{
-		// Not disguised
-		if (!player.isDisguised())
-		{
-			return;
-		}
-		// Null custom disguise
-		if (hubPlayer.getDisguise() == null)
-		{
-			return;
-		}
-		CustomDisguiseEffect customDisguiseEffect = hubPlayer.getDisguise().getEffect();
-		// No effect, we don't do anything more about it
-		if (customDisguiseEffect == null)
-		{
-			return;
-		}
-		//player.playEffect(player.getLocation(), customDisguiseEffect.build(), 0);
-	}
+        workWithDisguiseEffects(player, hubPlayer);
+        workLaunchpad(player, event.getTo());
+        autoTeleport(player, event);
+    }
 
-	/*private void workWithDoubleJump(BadblockPlayer player){
-		Location loc = player.getLocation();
-		Block block = loc.subtract(0.0D, 2.0D, 0.0D).getBlock();
+    private void autoTeleport(BadblockPlayer player, PlayerMoveEvent event) {
+        Location to = event.getTo();
+        if (to.getY() <= 0) {
+            event.setTo(to.getWorld().getSpawnLocation());
+        }
+    }
 
-		//TODO Double jump :)
-		if((player.getGameMode() == GameMode.ADVENTURE || player.getGameMode() == GameMode.SURVIVAL) && (block.getType() != Material.AIR) && (player.hasPermission("hub.doublejump"))){
-			player.setAllowFlight(true);
-		}
-	}*/
+    private void workLaunchpad(BadblockPlayer player, Location to) {
+        Location loc2 = to.clone().add(0, -1, 0);
+        Location loc3 = to.clone().add(0, -2, 0);
+        Block block1 = loc2.getBlock();
+        Block block2 = loc3.getBlock();
+        if (isLaunchpadBlock(block1) || isLaunchpadBlock(block2)) {
+            player.setVelocity(player.getLocation().getDirection().multiply(20).add(new Vector(0.5, 3, 0.5)));
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+    private boolean isLaunchpadBlock(Block block) {
+        return block.getType().equals(Material.WOOL) && block.getData() == 10;
+    }
+
+    private void workWithDisguiseEffects(BadblockPlayer player, HubPlayer hubPlayer) {
+        // Not disguised
+        if (!player.isDisguised()) {
+            return;
+        }
+        // Null custom disguise
+        if (hubPlayer.getDisguise() == null) {
+            return;
+        }
+        CustomDisguiseEffect customDisguiseEffect = hubPlayer.getDisguise().getEffect();
+        // No effect, we don't do anything more about it
+        if (customDisguiseEffect == null) {
+            return;
+        }
+        //player.playEffect(player.getLocation(), customDisguiseEffect.build(), 0);
+    }
 
 }
