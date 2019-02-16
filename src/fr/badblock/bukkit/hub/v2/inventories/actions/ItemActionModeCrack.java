85cbb44b5e75da6fe4c3b9fcf167abcae072f08d
xMalware <xmalware2@gmail.com>
2019-02-16 19:07:31 +0100

    premium/crack mode

---
+++ src/fr/badblock/bukkit/hub/v2/inventories/actions/ItemActionModeCrack.java
@@ -0,0 +1,34 @@
+package fr.badblock.bukkit.hub.v2.inventories.actions;
+
+import fr.badblock.api.common.utils.flags.GlobalFlags;
+import fr.badblock.bukkit.hub.v2.inventories.objects.CustomItemAction;
+import fr.badblock.bukkit.hub.v2.inventories.objects.CustomItemActionType;
+import fr.badblock.gameapi.players.BadblockPlayer;
+
+public class ItemActionModeCrack extends CustomItemAction
+{
+
+	@Override
+	public void execute(BadblockPlayer player, CustomItemActionType action, String actionData)
+	{
+		String spamKey = "player_mode_" + player.getName();
+
+		if (GlobalFlags.has(spamKey))
+		{
+			player.sendTranslatedMessage("hub.mode.pleasewait");
+			return;
+		}
+
+		// 10s antispam
+		GlobalFlags.set(spamKey, 5000);
+
+		if (player.getObject() != null)
+		{
+			player.getObject().addProperty("onlineMode", false);
+			player.saveGameData();
+		}
+
+		player.sendTranslatedMessage("hub.mode.nowcrack");
+	}
+
+}
